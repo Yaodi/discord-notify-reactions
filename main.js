e120ef98d29eb3fe540d94a42ab0022bdc5e9bd5
yaodi <yaodijr@gmail.com>
2020-06-22 14:40:47 -0500

    made it so that clock ins always fetch for data so it does not rely on cached data to work. also allows you to ping message links now as well

--- main.js
+++ main.js
@@ -12,34 +12,48 @@ client.once("ready", () => {
 });
 client.login(process.env.TOKEN);
 
-client.on("message", (message) => {
+client.on("message", async (message) => {
   if (message.mentions.users.size < countAts(message.content)) {
     messages.set(message.author.id, message);
-  } else if (message.content.includes("!clock in <@")) {
+  } else if (message.content.toLowerCase().includes("!clock in <@")) {
     let userID = message.mentions.users.keys().next().value;
-    message.channel.send(pingReactors(userID));
-  } else if (message.content.includes("!clock in")) {
-    message.channel.send(pingReactors(message.author.id));
+    let messageToSend = await findReactorsByUserID(userID);
+    message.channel.send(messageToSend);
+  } else if (
+    message.content.toLowerCase().includes("!clock in https://discordapp.com/")
+  ) {
+    let messageID = message.content.split("/").pop();
+    let fetchedMessage = await message.channel.messages.fetch(messageID, false);
+    let messageToSend = await pingReactors(fetchedMessage);
+    message.channel.send(messageToSend);
+  } else if (message.content.toLowerCase().includes("!clock in")) {
+    let messageToSend = await findReactorsByUserID(message.author.id);
+    message.channel.send(messageToSend);
   }
 });
 
-function pingReactors(userID) {
+function findReactorsByUserID(userID) {
   if (messages.has(userID)) {
     let pingedMessage = messages.get(userID);
-    let reactions = pingedMessage.reactions.cache;
-    let usersReacted = new Map();
-    reactions.forEach((reaction) => {
-      reaction.users.cache.forEach((value, key) => {
-        usersReacted.set(key, value);
-      });
-    });
-    let botMessage = "> " + pingedMessage.content + "\n";
-    usersReacted.forEach((user) => {
-      botMessage += ` <@${user.id}>`;
-    });
-    return usersReacted.size ? botMessage : noReactMessage;
+    return pingReactors(pingedMessage);
+  } else {
+    return errorMessage;
   }
-  return errorMessage;
+}
+
+async function pingReactors(message) {
+  let reactions = message.reactions.cache;
+  let usersReacted = new Set();
+  for (let reaction of reactions.values()) {
+    let users = await reaction.users.fetch();
+    users.forEach((value, key) => usersReacted.add(key));
+  }
+
+  let botMessage = "> " + message.content + "\n";
+  usersReacted.forEach((user) => {
+    botMessage += ` <@${user}>`;
+  });
+  return usersReacted.size ? botMessage : noReactMessage;
 }
 
 function countAts(string) {
